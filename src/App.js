4a094a0590ebd75b9590d3b9466d6f09ca6e1617
Lacina ZINA <zinalacina@gmail.com>
2019-01-09 14:35:03 +0900

    branch added

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React and changed branch
           </a>
         </header>
       </div>
